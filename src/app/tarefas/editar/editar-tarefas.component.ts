e028565c715588823912aebe15aa22fafed5113e
alexalins <alexa.glinsoo@gmail.com>
2021-07-05 19:57:28 -0300

    desenvolvendo o editar

--- src/app/tarefas/editar/editar-tarefas.component.ts
+++ src/app/tarefas/editar/editar-tarefas.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tarefa, TarefaService } from '../shared';
 
 @Component({
   selector: 'app-editar-tarefas',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarTarefasComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTarefa', { static: true }) formTarefa: NgForm; //para criar uma referencia
+  tarefa: Tarefa;
+
+  constructor(
+    private tarefaService: TarefaService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    //pegando o id que passa na rota
+    // + para passar pra numerico
+    const id = +this.route.snapshot.params["id"];
+    //bucando tarefa
+    this.tarefa = this.tarefaService.buscarPorId(id);
   }
 
+  atualizar(): void {
+    //se for valido
+    if(this.formTarefa.form.valid) {
+      //atualiza a tarefa
+      this.tarefaService.atualizar(this.tarefa);
+      //muda a rota
+      this.router.navigate(['/tarefas']);
+    }
+  }
 }
